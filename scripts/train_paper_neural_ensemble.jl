2465e298342f10c8a5932820c766dbb62107aa4f
ruiite <rilkil125@gmail.com>
2026-03-11 14:10:57 +0200

    added vae experiments

--- scripts/train_paper_neural_ensemble.jl
+++ scripts/train_paper_neural_ensemble.jl
@@ -1,19 +1,50 @@
 using ProbabilisticEnsembling
 
-const SESSIONS = [
-    "sessions/neural_ensemble/neural_ensemble_ETTh1_96.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh1_192.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh1_336.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh1_720.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh2_96.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh2_192.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh2_336.yaml",
-    "sessions/neural_ensemble/neural_ensemble_ETTh2_720.yaml",
-    "sessions/neural_ensemble/neural_ensemble_exchange_rate_96.yaml",
-    "sessions/neural_ensemble/neural_ensemble_exchange_rate_192.yaml",
-    "sessions/neural_ensemble/neural_ensemble_exchange_rate_336.yaml",
-    "sessions/neural_ensemble/neural_ensemble_exchange_rate_720.yaml",
+const NOIS_SESS = [
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_336.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_720.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_192.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_192.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_336.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_336.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_720.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_192.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_720.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_192.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_336.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_720.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_96.yaml",
 ]
+
+const SESSIONS = [
+    # "sessions/neural_ensemble/neural_ensemble_ETTh1_96.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh1_192.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh1_336.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh1_720.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh2_96.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh2_192.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh2_336.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_ETTh2_720.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_exchange_rate_96.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_exchange_rate_192.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_exchange_rate_336.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_exchange_rate_720.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_electricity_96.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_electricity_192.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_electricity_336.yaml",
+    # "sessions/neural_ensemble/neural_ensemble_electricity_720.yaml",
+    "sessions/neural_ensemble/neural_ensemble_traffic_96.yaml",
+    "sessions/neural_ensemble/neural_ensemble_traffic_192.yaml",
+    "sessions/neural_ensemble/neural_ensemble_traffic_336.yaml",
+    "sessions/neural_ensemble/neural_ensemble_traffic_720.yaml",]
+
+# for session in NOIS_SESS
+#     @info "noise"
+#     run_experiment(session)
+# end
 
 for session in SESSIONS
     @info "Training" session
