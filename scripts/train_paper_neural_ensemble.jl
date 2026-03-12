ce88034bd48726d40389194b3dc80a4a7236e7d5
ruiite <rilkil125@gmail.com>
2026-03-12 10:27:04 +0200

    vae expertiments for diagonal models

--- scripts/train_paper_neural_ensemble.jl
+++ scripts/train_paper_neural_ensemble.jl
@@ -1,22 +1,18 @@
 using ProbabilisticEnsembling
 
 const NOIS_SESS = [
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_336.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_96.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_720.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_192.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_192.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_336.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_96.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_336.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh2_720.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_exchange_rate_96.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_192.yaml",
-    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_ETTh1_720.yaml",
-    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_192.yaml",
-    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_336.yaml",
-    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_720.yaml",
-    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/noisy_experts/vae/noisy_experts_electricity_96.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh1_192.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh1_336.yaml",
+    # "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh2_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_exchange_rate_192.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_exchange_rate_336.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_exchange_rate_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh1_720.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh1_96.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh2_192.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh2_336.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_ETTh2_720.yaml",
+    "/Users/ruiite/projects/new_prob/probabilistic_ensemble_forecasting/sessions/dynamic_diagonal/vae/dynamic_diagonal_exchange_rate_720.yaml",
 ]
 
 const SESSIONS = [
@@ -41,16 +37,16 @@ const SESSIONS = [
     "sessions/neural_ensemble/neural_ensemble_traffic_336.yaml",
     "sessions/neural_ensemble/neural_ensemble_traffic_720.yaml",]
 
-# for session in NOIS_SESS
-#     @info "noise"
-#     run_experiment(session)
-# end
-
-for session in SESSIONS
-    @info "Training" session
-    run_neural_ensemble_experiment(session)
-    @info "Done" session
+for session in NOIS_SESS
+    @info "noise"
+    run_experiment(session)
 end
+
+# for session in SESSIONS
+#     @info "Training" session
+#     run_neural_ensemble_experiment(session)
+#     @info "Done" session
+# end
 
 # After training, copy results to paper/results/neural_ensemble/ with correct names:
 #   ETTh1_h{H}_OT_neural_ensemble.jld2
