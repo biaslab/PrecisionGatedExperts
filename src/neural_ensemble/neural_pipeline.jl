e81469931ba0f686eb9897dc4de353bdaa0de6ce
ruiite <rilkil125@gmail.com>
2026-04-19 22:04:08 +0300

    fix: calculate metrics in pipeline for univariate variant for neural ensemble

--- src/neural_ensemble/neural_pipeline.jl
+++ src/neural_ensemble/neural_pipeline.jl
@@ -327,13 +327,13 @@ function evaluate_neural_ensemble(
 )
     n_test = size(predictions_test_vec, 2)
 
-    ensemble_mean = hcat(
+    ensemble_mean_all = hcat(
         [
             moe_predict(predictions_test_vec[:, j], gating, ps, st, features_test[j])
             for j = 1:n_test
         ]...,
     )
-    ensemble_std = sqrt.(
+    ensemble_std_all = sqrt.(
         hcat(
             [
                 moe_var(predictions_test_vec[:, j], gating, ps, st, features_test[j])
@@ -354,11 +354,11 @@ function evaluate_neural_ensemble(
     gating_weights =
         hcat([gating_probs(gating, ps, st, features_test[j]) for j = 1:n_test]...)
 
-    # Evaluate on target column only
-    y_eval = y_test_mat[col_idx:col_idx, :]
-    ensemble_eval = ensemble_mean[col_idx:col_idx, :]
+    # Evaluate on target column only (same semantics as shared_pipeline univariate path)
+    ensemble_mean = vec(ensemble_mean_all[col_idx, :])
+    ensemble_std = vec(ensemble_std_all[col_idx, :])
     y_test = [y_test_mat[col_idx, j] for j = 1:n_test]
-    mse_terms = vec((ensemble_eval .- y_eval) .^ 2)
+    mse_terms = (ensemble_mean .- y_test) .^ 2
     nll_terms = collect(
         map((y_dist) -> logpdf(y_dist[2], y_dist[1]), zip(y_test, normal_predictions)),
     )
@@ -381,13 +381,13 @@ function evaluate_neural_ensemble(
     )
 
     ensemble_metrics = (
-        mse = mse_mv(ensemble_eval, y_eval),
-        mae = mae_mv(ensemble_eval, y_eval),
-        rmse = rmse_mv(ensemble_eval, y_eval),
+        mse = mse(ensemble_mean, y_test),
+        mae = mae(ensemble_mean, y_test),
+        rmse = rmse(ensemble_mean, y_test),
         mse_std = std(mse_terms),
-        r2 = r2_mv(ensemble_eval, y_eval),
-        mape = mape_mv(ensemble_eval, y_eval),
-        smape = smape_mv(ensemble_eval, y_eval),
+        r2 = r2(ensemble_mean, y_test),
+        mape = mape(ensemble_mean, y_test),
+        smape = smape(ensemble_mean, y_test),
         nll = mean(nll_terms),
         nll_std = std(nll_terms),
         ci95_target_overlap = ci95_target_overlap,
